8830a3a8a3a9f4d779c5a0fd09b4f722edaab91c
abodi99 <78203043+abodi99@users.noreply.github.com>
2023-10-14 16:24:13 +0200

    Update test_api.py

--- test/test_api.py
+++ test/test_api.py
@@ -3,7 +3,7 @@ import json
 import requests
 
 
-from calculator.calculator_helper import CalculatorHelper
+from Devops.calculator_helper import CalculatorHelper
 from calculator_api.configuration import Configuration
 from calculator_api.api_client import ApiClient
 from calculator_api.models.calculation import Calculation
