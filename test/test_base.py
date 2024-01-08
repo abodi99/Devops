0000550adc28f48da171b450d9a750add641c45a
abodi99 <78203043+abodi99@users.noreply.github.com>
2024-01-08 22:57:36 +0100

    Delete test directory

--- test/test_base.py
+++
@@ -1,9 +0,0 @@
-from calculator_helper import CalculatorHelper
-import pytest
-
-class BaseTest:
-    def setup(self):
-        self.calc =  CalculatorHelper()
-
-    def teardown(self):
-        return self.calc
