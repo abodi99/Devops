6366a8cb458fcfc6ddcf32fc4c7c0c85d467c4bc
abodi99 <78203043+abodi99@users.noreply.github.com>
2024-01-10 20:35:10 +0100

    Update __init__.py

--- calculator/api/__init__.py
+++ calculator/api/__init__.py
@@ -3,4 +3,4 @@ from __future__ import absolute_import
 # flake8: noqa
 
 # import apis into api package
-from calculator_api.api.calculator_api import CalculatorApi
+from calculator.api.calculator_api import CalculatorApi
