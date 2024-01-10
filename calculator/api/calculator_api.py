3add003076caf3d7028214077cf8c9066ca36816
abodi99 <78203043+abodi99@users.noreply.github.com>
2024-01-10 20:33:56 +0100

    Update calculator_api.py

--- calculator/api/calculator_api.py
+++ calculator/api/calculator_api.py
@@ -18,7 +18,7 @@ import re  # noqa: F401
 # python 2 and python 3 compatibility library
 import six
 
-from calculator_api.api_client import ApiClient
+from calculator.api_client import ApiClient
 
 
 class CalculatorApi(object):
